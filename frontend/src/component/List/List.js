8b99651dd710d4a3f9e4e78a05ab1a3e6d492a24
Ni_Chujie <ncj19991213@126.com>
2019-01-16 10:10:31 +0800

    Axios Test

--- frontend/src/component/List/List.js
+++ frontend/src/component/List/List.js
@@ -5,17 +5,21 @@ class List extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      todoList: []
+      todoList: [1, 2, 3]
     };
     this.handleClick = this.handleClick.bind(this);
   }
 
-  handleClick = () => {
+  handleClick(e) {
+    e.preventDefault();
     axios({
       method: 'GET',
       url: 'http://127.0.0.1:8000/todo/',
-    }).then(function (response) {
+    }).then((response) => {
       console.log(response.data);
+      this.setState({
+        todoList: response.data
+      })
     });
   };
 
@@ -23,7 +27,7 @@ class List extends Component {
     return (
       <div>
         <h1>List</h1>
-        <button onClick={this.handleClick}>233</button>
+        <button onClick={(e) => this.handleClick(e)}>233</button>
         <h2>{this.state.todoList}</h2>
       </div>
     );
