0a8b8f80d5cd70a542d16bc3cdf5cdac31ea4955
Cheney_Ni <ncj19991213@126.com>
2020-01-16 00:01:27 +0800

    convert TodoCategory to FC

--- frontend/src/components/TodoCategory.tsx
+++ frontend/src/components/TodoCategory.tsx
@@ -7,34 +7,33 @@ interface IMenuClick {
   key: string
 }
 
-class TodoCategory extends Component<any, any> {
-  handleClick = (e: IMenuClick) => {
-    TodoStore.showType = e.key;
-  };
+let handleClick = (e: IMenuClick) => {
+  TodoStore.showType = e.key;
+};
 
-  render() {
-    return (
-      <Menu
-        theme="dark"
-        defaultSelectedKeys={['undone']}
-        mode="inline"
-        onClick={this.handleClick}
-      >
-        <Menu.Item key="undone">
-          <Icon type="clock-circle"/>
-          <span>未完成</span>
-        </Menu.Item>
-        <Menu.Item key="done">
-          <Icon type="check-circle"/>
-          <span>已完成</span>
-        </Menu.Item>
-        <Menu.Item key="expired">
-          <Icon type="exclamation-circle"/>
-          <span>已过期</span>
-        </Menu.Item>
-      </Menu>
-    );
-  }
-}
+const TodoCategory: React.FC = () => {
+  return (
+    <Menu
+      theme="dark"
+      defaultSelectedKeys={['undone']}
+      mode="inline"
+      onClick={handleClick}
+    >
+      <Menu.Item key="undone">
+        <Icon type="clock-circle"/>
+        <span>未完成</span>
+      </Menu.Item>
+      <Menu.Item key="done">
+        <Icon type="check-circle"/>
+        <span>已完成</span>
+      </Menu.Item>
+      <Menu.Item key="expired">
+        <Icon type="exclamation-circle"/>
+        <span>已过期</span>
+      </Menu.Item>
+    </Menu>
+  );
+};
+
 
 export default TodoCategory;
